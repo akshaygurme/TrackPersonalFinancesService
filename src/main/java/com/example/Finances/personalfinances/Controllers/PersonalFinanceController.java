a0b905e3820b7b8d42348fc327882b8ceb80ec48
akshaygurme <42416798+akshaygurme@users.noreply.github.com>
2024-11-18 00:27:26 +0530

    Added Hygiene conditions

--- src/main/java/com/example/Finances/personalfinances/Controllers/PersonalFinanceController.java
+++ src/main/java/com/example/Finances/personalfinances/Controllers/PersonalFinanceController.java
@@ -1,6 +1,9 @@
 package com.example.Finances.personalfinances.Controllers;
 
 import com.example.Finances.personalfinances.Models.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.log4j.Log4j2;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Controller;
@@ -24,17 +27,22 @@ import java.io.Reader;
 public class PersonalFinanceController {
     @RequestMapping("/fetchData")
     @ResponseBody
-    public String hello() {
+    public String hello() throws JsonProcessingException {
         String filePath = "src\\main\\resources\\Nov2024InputCSV.csv"; // Replace with your file path
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
         //Read excel
         //Store it in POJO
         var allTransactions = convertCSVtoPOJO(filePath);
         log.info("----------------All Transactions {}",allTransactions);
+
         //Process this data into different objects so easy to handle
         var hygieneTransactions = hygieneAllTransactions(allTransactions);
-        log.info("----------------All Hygiened Transactions {}",hygieneTransactions);
-        //Print data
 
+        //Print data
+        String json = mapper.writeValueAsString(hygieneTransactions);
+        log.info("----------------All Hygiened Transactions {}",json);
 
         return "Hello World";
     }
@@ -48,36 +56,9 @@ public class PersonalFinanceController {
             Investment investment = new Investment();
             Expense expense = new Expense();
 
-            bankBalance.setAxisBalance(Double.parseDouble(transaction.getAxisBalance()));
-            bankBalance.setSbiBalance(Double.parseDouble(transaction.getSbiBalance()));
-            bankBalance.setHdfcBalance(Double.parseDouble(transaction.getHdfcBalance()));
-            bankBalance.setTotalCash(Double.parseDouble(transaction.getTotalCash()));
-
-
-            investment.setEmergencyFund(Double.parseDouble(transaction.getEmergencyFund()));
-            investment.setInvoiceDiscounting(Double.parseDouble(transaction.getInvoiceDiscounting()));
-            investment.setUsStocks(Double.parseDouble(transaction.getUsStocks()));
-            investment.setCorporateBonds(Double.parseDouble(transaction.getCorporateBonds()));
-            investment.setCorporateBankFD(Double.parseDouble(transaction.getCorporateBankFD()));
-            investment.setNps(Double.parseDouble(transaction.getNps()));
-            investment.setElss(Double.parseDouble(transaction.getElss()));
-            investment.setStocks(Double.parseDouble(transaction.getStocks()));
-            investment.setMutualFunds(Double.parseDouble(transaction.getMutualFunds()));
-            investment.setGoldMutualFund(Double.parseDouble(transaction.getGoldMutualFund()));
-            investment.setPhysicalGold(Double.parseDouble(transaction.getPhysicalGold()));
-            investment.setSgb(Double.parseDouble(transaction.getSgb()));
-            investment.setCrypto(Double.parseDouble(transaction.getCrypto()));
-            investment.setRealEstate(Double.parseDouble(transaction.getRealEstate()));
-
-
-            expense.setStudy(Double.parseDouble(transaction.getStudy()));
-            expense.setDailyNeeds(Double.parseDouble(transaction.getDailyNeeds()));
-            expense.setInsurance(Double.parseDouble(transaction.getInsurance()));
-            expense.setWants(Double.parseDouble(transaction.getWants()));
-            expense.setLend(Double.parseDouble(transaction.getLend()));
-            expense.setConstruction(Double.parseDouble(transaction.getConstruction()));
-            expense.setDonation(Double.parseDouble(transaction.getDonation()));
-            expense.setOther(Double.parseDouble(transaction.getOther()));
+            bankBalance = buildBankBalance(transaction);
+            investment = buildInvestment(transaction);
+            expense = buildExpense(transaction);
 
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yy");
 
@@ -88,15 +69,58 @@ public class PersonalFinanceController {
             hygieneTransaction.setBankName(transaction.getBankName());
             hygieneTransaction.setTransactionTitle(transaction.getTransactionTitle());
             hygieneTransaction.setBankBalance(bankBalance);
-            hygieneTransaction.setSalary(Double.parseDouble(transaction.getSalary()));
-            hygieneTransaction.setReceivedAmount(Double.parseDouble(transaction.getReceivedAmount()));
+            hygieneTransaction.setSalary(Double.parseDouble(transaction.getSalary().isEmpty() ? "0" : transaction.getSalary()));
+            hygieneTransaction.setReceivedAmount(Double.parseDouble(transaction.getReceivedAmount().isEmpty() ? "0" : transaction.getReceivedAmount()));
             hygieneTransaction.setInvestments(investment);
-            hygieneTransaction.setReturns(Double.parseDouble(transaction.getReturns()));
+            hygieneTransaction.setReturns(Double.parseDouble(transaction.getReturns().isEmpty() ? "0" : transaction.getReturns()));
             hygieneTransaction.setExpenses(expense);
 
             hygieneTransactions.add(hygieneTransaction);
         }
         return hygieneTransactions;
+    }
+
+    private BankBalance buildBankBalance(AllTransactionEntries transaction) {
+        BankBalance bankBalance = new BankBalance();
+        bankBalance.setAxisBalance(Double.parseDouble(transaction.getAxisBalance().isEmpty() ? "0" : transaction.getAxisBalance()));
+        bankBalance.setSbiBalance(Double.parseDouble(transaction.getSbiBalance().isEmpty() ? "0" : transaction.getSbiBalance()));
+        bankBalance.setHdfcBalance(Double.parseDouble(transaction.getHdfcBalance().isEmpty() ? "0" : transaction.getHdfcBalance()));
+        bankBalance.setTotalCash(Double.parseDouble(transaction.getTotalCash().isEmpty() ? "0" : transaction.getTotalCash()));
+        return bankBalance;
+    }
+
+    private Expense buildExpense(AllTransactionEntries transaction) {
+        Expense expense = new Expense();
+        expense.setStudy(Double.parseDouble(transaction.getStudy().isEmpty() ? "0" : transaction.getStudy()));
+        expense.setDailyNeeds(Double.parseDouble(transaction.getDailyNeeds().isEmpty() ? "0" : transaction.getDailyNeeds()));
+        expense.setInsurance(Double.parseDouble(transaction.getInsurance().isEmpty() ? "0" : transaction.getInsurance()));
+        expense.setWants(Double.parseDouble(transaction.getWants().isEmpty() ? "0" : transaction.getWants()));
+        expense.setLend(Double.parseDouble(transaction.getLend().isEmpty() ? "0" : transaction.getLend()));
+        expense.setConstruction(Double.parseDouble(transaction.getConstruction().isEmpty() ? "0" : transaction.getConstruction()));
+        expense.setDonation(Double.parseDouble(transaction.getDonation().isEmpty() ? "0" : transaction.getDonation()));
+        expense.setOther(Double.parseDouble(transaction.getOther().isEmpty() ? "0" : transaction.getOther()));
+        return expense;
+    }
+
+    private Investment buildInvestment(AllTransactionEntries transaction) {
+
+        Investment investment = new Investment();
+        investment.setEmergencyFund(Double.parseDouble(transaction.getEmergencyFund().isEmpty() ? "0" : transaction.getEmergencyFund()));
+        investment.setInvoiceDiscounting(Double.parseDouble(transaction.getInvoiceDiscounting().isEmpty() ? "0" : transaction.getInvoiceDiscounting()));
+        investment.setUsStocks(Double.parseDouble(transaction.getInvoiceDiscounting().isEmpty() ? "0" : transaction.getInvoiceDiscounting()));
+        investment.setCorporateBonds(Double.parseDouble(transaction.getCorporateBonds().isEmpty() ? "0" : transaction.getCorporateBonds()));
+        investment.setCorporateBankFD(Double.parseDouble(transaction.getCorporateBankFD().isEmpty() ? "0" : transaction.getCorporateBankFD()));
+        investment.setNps(Double.parseDouble(transaction.getNps().isEmpty() ? "0" : transaction.getNps()));
+        investment.setElss(Double.parseDouble(transaction.getElss().isEmpty() ? "0" : transaction.getElss()));
+        investment.setStocks(Double.parseDouble(transaction.getStocks().isEmpty() ? "0" : transaction.getStocks()));
+        investment.setMutualFunds(Double.parseDouble(transaction.getMutualFunds().isEmpty() ? "0" : transaction.getMutualFunds()));
+        investment.setGoldMutualFund(Double.parseDouble(transaction.getGoldMutualFund().isEmpty() ? "0" : transaction.getGoldMutualFund()));
+        investment.setPhysicalGold(Double.parseDouble(transaction.getPhysicalGold().isEmpty() ? "0" : transaction.getPhysicalGold()));
+        investment.setSgb(Double.parseDouble(transaction.getSgb().isEmpty() ? "0" : transaction.getSgb()));
+        investment.setCrypto(Double.parseDouble(transaction.getCrypto().isEmpty() ? "0" : transaction.getCrypto()));
+        investment.setRealEstate(Double.parseDouble(transaction.getRealEstate().isEmpty() ? "0" : transaction.getRealEstate()));
+
+        return investment;
     }
 
     public List<AllTransactionEntries> convertCSVtoPOJO(String filePath){
